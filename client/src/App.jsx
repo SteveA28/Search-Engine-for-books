714892d60075e8557841a3befcb8d137345f1b09
SteveA28 <alonsosteven32@gmail.com>
2024-03-17 19:37:04 -0400

    fixed code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,14 @@
-// import React from "react";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import SearchBooks from "./pages/SearchBooks";
-// import SavedBooks from "./pages/SavedBooks";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import { Outlet } from "react-router-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import './App.css';
+import { Outlet } from 'react-router-dom';
 
-const httpLink = createHttpLink({
-  // uri: "http://localhost:3001/graphql",
-  uri: "/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("id_token");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <>
       <Navbar />
       <Outlet />
-    </ApolloProvider>
+    </>
   );
 }
 
--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,14 +2,11 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-// import { loginUser } from '../utils/API';
+import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
-import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../utils/mutations";
 
 const LoginForm = () => {
-  const [loginUser] = useMutation(LOGIN_USER);
-  const [ userFormData, setUserFormData] = useState({ email: '', password: ''});
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -29,20 +26,13 @@ const LoginForm = () => {
     }
 
     try {
-      // const response = await loginUser(userFormData);
-      const response = await loginUser({
-        variables: {
-          email: userFormData.email,
-          password: userFormData.password
-        }
-      })
+      const response = await loginUser(userFormData);
 
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
 
-      // const { token, user } = await response.json();
-      const { token, user } = response.data.login;
+      const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
     } catch (err) {
